3fa814f1c24ec9ee046c4f3110695ec0cccb69a4
Abdallah Moubarak <abdallah.moubarak02@gmail.com>
2023-05-26 17:43:58 +0300

    changes

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,35 @@
-export default function Home() {
-  return <main></main>;
+import { fetchProducts } from "@/lib/products";
+import {
+  JSXElementConstructor,
+  PromiseLikeOfReactNode,
+  ReactElement,
+  ReactFragment,
+  ReactPortal,
+} from "react";
+
+export default async function Home() {
+  const products: any = await fetchProducts();
+  console.log(products);
+
+  return (
+    <main>
+      <div>Hello world</div>
+      {/* {products?.map(
+        (product: {
+          name:
+            | string
+            | number
+            | boolean
+            | ReactElement<any, string | JSXElementConstructor<any>>
+            | ReactFragment
+            | ReactPortal
+            | PromiseLikeOfReactNode
+            | null
+            | undefined;
+        }) => (
+          <div>{product.name}</div>
+        )
+      )} */}
+    </main>
+  );
 }
